9d28554c6307ab4873693eb9c8a3d6503a04b130
MatthijsBork <mborkhuis@st.noorderpoort.nl>
2026-03-24 21:36:28 +0100

    opzet server

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+    
