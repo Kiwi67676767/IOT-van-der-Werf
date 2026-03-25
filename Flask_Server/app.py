707e692920d9a0a15acb06d3b6f07c5cec992ef5
MatthijsBork <mborkhuis@st.noorderpoort.nl>
2026-03-25 22:54:06 +0100

    refactor met Dependency Injection

--- Flask_Server/app.py
+++ Flask_Server/app.py
@@ -1,31 +1,29 @@
 import os
 from flask import Flask, render_template
-from datetime import datetime
 from extensions import db
-from sqlalchemy.orm import Mapped, mapped_column
 from models import Meting
+from routes import routes
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+def create_app():
+    app = Flask(__name__)
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    db.init_app(app)
 
+    # Blueprint (routes) registreren
+    app.register_blueprint(routes)
 
+    # 5. Database tabellen aanmaken
+    with app.app_context():
+        db.create_all()
 
-@app.route("/")
-def index() -> str:
-    """Homepage route"""
-    metingen = db.session.execute(db.select(Meting)).scalars().all()
-    return render_template("index.html", metingen=metingen) # betekent dat de data die hier binnenkomt "metingen" ook weer worden gegeven in de "index.html" als "metingen"
+    return app
 
-
+# Start de server alleen als dit bestand direct wordt uitgevoerd
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
 
-@app.route("/api/metingen", methods=["GET"])
-def api_metingen():
-    """API route to get all measurements"""
-    metingen = db.session.execute(db.select(Meting)).scalars().all()
-    return [{"id": m.id, "hoogte": m.hoogte, "tijd_van_meting": m.tijd_van_meting.isoformat()} for m in metingen]
--- routes.py
+++
@@ -1,2 +0,0 @@
-from flask import Blueprint, request, jsonify
-from models import db
