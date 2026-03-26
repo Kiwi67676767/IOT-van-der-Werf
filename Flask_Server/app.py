09e81179e3b75706d2cc6bdd30cb693f909bfb9c
“Brent” <b.oldenbeuving@st.hanze.nl>
2026-03-26 19:59:55 +0100

    Host wijziging

--- Flask_Server/app.py
+++ Flask_Server/app.py
@@ -23,4 +23,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    socketio.run(app, debug=True, host='127.0.0.1', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
