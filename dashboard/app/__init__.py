e43e48d17876caca168f96aa85ba5282cf8fb937
MatthijsBork <m.borkhuis@st.hanze.nl>
2026-04-22 22:04:02 +0200

    dashboard opzet

---
+++ dashboard/app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'heel-geheim'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'main.login'
+    login_manager.init_app(app)
+
+    from .models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    # Maak de database aan als die nog niet bestaat
+    with app.app_context():
+        db.create_all()
+
+    return app
